ac81088419da5072300f3820b2f84fdd8a1bbd53
ajayvenkateshps <149811885+ajayvenkateshps@users.noreply.github.com>
2023-12-07 16:43:01 +0530

    Add files via upload

---
+++ Undirected_graph_using_array.c
@@ -0,0 +1,178 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAX_V 100
+
+
+
+void addEdge(int arr[MAX_V][MAX_V], int src, int des)
+{
+	arr[src][des] = 1;
+	
+	arr[des][src] = 1;  //delete this line .. that is directed graph .
+}
+
+void printGraph(int arr[MAX_V][MAX_V], int vertices)
+{
+	printf("Adjacency matric :\n\n");
+	
+	for(int i=0; i<vertices; ++i)
+	{
+		for(int j=0; j<vertices; ++j)
+		{
+			printf("%d  ", arr[i][j]);		
+		}	
+		printf("\n");
+	}
+		
+}
+
+int main()
+{
+	int arr[MAX_V][MAX_V];
+	int vertices ,i,j,choice,src,des;
+	
+	
+	printf("Enter the number of vertices:");
+	scanf("%d",&vertices);
+	
+	for(i=0; i<vertices; ++i)
+	{
+		for(j=0; j<vertices; ++j)
+		{
+			arr[MAX_V][MAX_V]=0;		
+		}
+	}
+	
+	while (choice !=3 )
+	{
+		
+		printf("\n\t\tMAIN MENU:\n1. Add Edge\n2. Print Graph\n3. Exit\n");
+		
+		printf("Enter you choice : ");
+		scanf("%d",&choice);
+		
+		
+		switch(choice)
+		{
+			case 1 : 
+			
+				printf("Enter the Source vertices (starts with 0): ");
+				scanf("%d", &src);
+				
+				printf("Enter the Destination vertices(vertices-1) : ");
+				scanf("%d", &des);
+				
+				if(src >=0 && src < vertices && des >=0 && des >=0 &&des < vertices)
+				{
+					addEdge(arr,src, des);
+					printf("Edge added successfully");	
+				}
+				else
+				{
+					printf("Invalid vertices\n");
+				}
+				
+				break;
+				
+				
+			case 2 : 
+				
+				printGraph(arr, vertices);
+				break;
+				
+			case 3 : 
+				
+				exit(0);
+				break;
+				
+			default : 
+				 printf("Invalid choice . enter a valid choice\n");			
+		}
+		
+	}	
+	return 0 ;
+}
+
+/*
+
+Output:
+
+	Enter the number of vertices:5
+
+		MAIN MENU:
+		
+		1. Add Edge
+		2. Print Graph
+		3. Exit
+		
+		Enter you choice : 2
+		Adjacency matric :
+
+			0  0  0  0  0  
+			0  0  0  0  0  
+			0  0  0  0  0  
+			0  0  0  0  0  
+			0  0  0  0  0  
+
+		MAIN MENU:
+		
+		1. Add Edge
+		2. Print Graph
+		3. Exit
+		
+		Enter you choice : 1
+		
+		Enter the Source vertices (starts with 0): 3
+		Enter the Destination vertices(vertices-1) : 2
+		Edge added successfully
+		
+		MAIN MENU:
+		
+		1. Add Edge
+		2. Print Graph
+		3. Exit
+		
+		Enter you choice : 2
+		Adjacency matric :
+
+			0  0  0  0  0  
+			0  0  0  0  0  
+			0  0  0  1  0  
+			0  0  1  0  0  
+			0  0  0  0  0  
+
+		MAIN MENU:
+		
+		1. Add Edge
+		2. Print Graph
+		3. Exit
+		
+		Enter you choice : 1
+		Enter the Source vertices (starts with 0): 1
+		Enter the Destination vertices(vertices-1) : 4
+		Edge added successfully
+		
+		MAIN MENU:
+		
+		1. Add Edge
+		2. Print Graph
+		3. Exit
+		
+		Enter you choice : 2
+		Adjacency matric :
+
+			0  0  0  0  0  
+			0  0  0  0  1  
+			0  0  0  1  0  
+			0  0  1  0  0  
+			0  1  0  0  0  
+
+
+*/
+
+
+
+
+
+
