b484007bd9c920fd67a02aaa3808938c8f2cbc90
ajay <149811885+ajay0118@users.noreply.github.com>
2023-12-13 11:03:48 +0530

    Add files via upload

---
+++ In_degree_out_degree.c
@@ -0,0 +1,156 @@
+// to check indegree and outdegree of the node :
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAX_V 100
+
+int arr[MAX_V][MAX_V];
+int vertices,i,j,choice,source,destination;
+	
+
+
+void addEdge(int source, int destination)
+{
+	if(source>=0 && source<vertices && destination>=0 && destination <vertices)
+	{
+		
+		arr[source][destination]=1;
+		printf("Edge added successfully..!");
+	}
+	else
+	{
+		printf("Invalid vertices..!\n");
+	}
+}
+
+void deleteEdge(int source, int destination)
+{
+	if(source>=0 && source<vertices && destination>=0 && destination <vertices)
+	{
+		arr[source][destination]=0;
+	}
+	else
+	{
+		printf("Invalid vertices..!");
+	}
+}
+void indegree(int node)
+{
+	int count= 0 ;
+	if(node<0 || node>vertices)
+	{
+		printf("\nOut of Range");
+	}
+	else
+	{
+		for(int j=0;j<vertices;j++)
+		{
+			if(arr[j][node]!=0)
+			{
+				count+=1;
+			}
+		}
+		printf("\nIndegree of the node %d is %d",node,count);
+	}
+}
+
+void outdegree(int node)
+{
+	int count = 0;
+	if(node<0 || node>vertices)
+	{
+		printf("\nOut of range..!");
+	}
+	else
+	{
+		for(j=0;j<vertices;j++)
+		{
+			if(arr[node][j]!=0)
+			{
+				count+=1;
+			}
+		}
+		printf("\nOutdegree of the node %d is %d",node,count);
+	}
+}
+
+void printGraph()
+{
+
+		
+	printf("Graph Matrix : \n  ");
+	for(i = 0;i<vertices;i++)
+	{
+		printf("%d ",i);
+	}
+	printf("\n");
+	for(i=0;i<vertices;i++)
+	{printf("%d ",i);
+		for(j=0;j<vertices;j++)
+		{
+			printf("%d ",arr[i][j]);
+		}
+		printf("\n");
+	}
+	
+}
+
+
+
+int main()
+{	
+	printf("Enter the number of vertices:");
+	scanf("%d",&vertices);
+	
+	for(i=0;i<vertices;i++)
+	{
+		for(j = 0 ; j<vertices;j++)
+		{
+			arr[i][j]=0;
+		}
+	}
+	while(choice!=7)
+	{
+		printf("\n\t\tMAIN MENU\n1.Add Edge\n2.Delete Edge\n3.Print Graph\n4.Indegree\n5.Out degree\n6.Exit\n");
+		
+		printf("Enter your choice:");
+		scanf("%d",&choice);
+		
+		switch(choice)
+		{
+			case 1:
+				printf("\nEnter the source vertex: ");
+				scanf("%d",&source);
+				printf("\nEnter the destination vertex: ");
+				scanf("%d",&destination);
+				addEdge(source,destination);
+				break;
+			case 2:
+				printf("\nEnter the source vertex: ");
+				scanf("%d",&source);
+				printf("\nEnter the destination vertex: ");
+				scanf("%d",&destination);
+				deleteEdge(source,destination);
+				break;
+			case 3:
+				printGraph();
+				break;
+			case 4:				printf("\nEnter the vertex: ");
+				scanf("%d",&source);
+				indegree(source);
+				break;
+			case 5:
+				printf("\nEnter the vertex: ");
+				scanf("%d",&source);
+				outdegree(source);
+				break;
+			case 6:
+				exit(0);
+			default:
+				printf("\nEnter valid input");
+		}
+	}
+}
+
+
